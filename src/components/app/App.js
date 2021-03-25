e707fe3c5c0bed6625b98fb47f1a5dcfdfea6c1b
Ajay <ajayaj7397@psnacet.edu.in>
2021-03-25 15:34:37 +0530

    removed register form

---
+++ src/components/app/App.js
@@ -0,0 +1,35 @@
+import Header from '../header/Header'
+import Realtos from '../realtos/Realtos'
+import Features from '../features/Features'
+import Footer from '../footer/Footer'
+import Homes from '../homes/Homes'
+import StoryContent from '../story/StoryContent'
+import StoryPicturs from '../story/StoryPicture'
+import Gallery from '../gallery/Gallery'
+import './App.scss'
+
+
+
+function App() {
+  return (
+     <div className="container"> 
+      <Header />
+      
+      <Realtos />
+
+      <Features />
+
+      <StoryPicturs />
+
+      <StoryContent />
+
+      <Homes />
+
+      <Gallery />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
